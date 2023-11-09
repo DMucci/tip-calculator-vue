03a7c2525901fe218a7478497e575a6e321a5846
Dmucci <dmucci93@gmail.com>
2023-11-09 10:28:10 +0100

    finished

---
+++ app.js
@@ -0,0 +1,26 @@
+import {
+    createApp,
+    ref,
+  } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+
+createApp ({
+    setup(){
+       const billAmount = ref("");
+       const percentageTip = ref("");
+       const tipAmount = ref("");
+       const total = ref("");
+
+       const calculate = () => {
+            tipAmount.value = billAmount.value * percentageTip.value/100;
+            total.value = billAmount.value+tipAmount.value;
+    }
+
+    return {
+        billAmount,
+        percentageTip,
+        tipAmount,
+        total,
+        calculate,
+    };
+}
+}).mount("#app");
